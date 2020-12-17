9eeda8138066d17b440d014e3b55bc0a3d6eef57
Dominic Thorn <dominic.thorn@gmail.com>
2020-12-17 20:02:16 +0000

    Shrink and containerise backend

--- backend/main.py
+++ backend/main.py
@@ -5,7 +5,6 @@ from dotenv import load_dotenv
 from fastai.basics import load_learner
 from fastai.learner import Learner
 from fastapi import FastAPI, File
-from google.cloud import storage
 from pydantic import BaseModel
 
 BASE_DIRECTORY = Path(__file__).parent.absolute()
@@ -23,13 +22,16 @@ class PredictOut(BaseModel):
 
 @app.get("/")
 def read_root():
-    return {"message": "Aldentefier ready - visit /docs or /redoc for API documentation."}
+    return {
+        "message": "Aldentefier ready - visit /docs or /redoc for API documentation."
+    }
 
 
 @app.post("/predict", response_model=PredictOut)
 def predict_image(
     image: bytes = File(
-        ..., description="Image sent to model for class prediction - required as binary."
+        ...,
+        description="Image sent to model for class prediction - required as binary.",
     )
 ):
     model = load_learner(ABSOLUTE_MODEL_PATH)
@@ -37,23 +39,5 @@ def predict_image(
     return {"predicted_class": pred[0], "likelihood": max(pred[2])}
 
 
-def download_model(bucket_name, model_name, destination_file_name):
-    storage_client = storage.Client()
-    bucket = storage_client.bucket(bucket_name)
-
-    blob = bucket.blob(model_name)
-    blob.download_to_filename(destination_file_name)
-
-
-def main(relative_path, absolute_path):
-    if not Path(absolute_path).is_file():
-        print(f"Downloading model to: {absolute_path}...")
-        download_model(os.environ.get("GSBUCKET"), relative_path, str(absolute_path))
-        print("Complete.")
-    else:
-        print(f"Found model: {absolute_path}")
-
-
 if __name__ == "__main__":
-    main(RELATIVE_MODEL_PATH, ABSOLUTE_MODEL_PATH)
-    uvicorn.run("main:app", host="0.0.0.0", port=8080, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=8080)
